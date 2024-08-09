2b74e7bdc05bfb7bdfe72404d33335aaa4d94ccb
Pedro Henrique Nicchio Kerkoff <160201116+Nicchorff@users.noreply.github.com>
2024-08-09 10:06:28 -0300

    build: adding bootstrap

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import './App.css';
 import  Home  from './components/Home';
 import Sobre from './components/Sobre';
 import Aluno from './components/Alunos';
+import { BrowserRouter , Routes , Route , Link } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <ul>
-        <li>Pagina inicial</li>
-        <li>Cadastro de alunos</li>
-        <li>Sobre</li>
-      </ul>
+      <BrowserRouter>
+        <ul>
+          <li><Link to="/">Pagina inicial</Link></li>
+          <li><Link to="/alunos">Cadastro de alunos</Link></li>
+          <li><Link to="/sobre">Sobre </Link></li>
+        </ul>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/alunos" element={<Aluno />} />
+          <Route path="/sobre" element={<Sobre />} />
+        </Routes>
+      </BrowserRouter>  
+
     </div>
   );
 }
