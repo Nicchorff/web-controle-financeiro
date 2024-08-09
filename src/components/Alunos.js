d066805c46b04ded7c32917fcc9b7cfa0dfe6068
Pedro Henrique Nicchio Kerkoff <160201116+Nicchorff@users.noreply.github.com>
2024-08-09 10:36:44 -0300

    feat: tabela para aluno

--- src/components/Alunos.js
+++ src/components/Alunos.js
@@ -1,10 +1,36 @@
-function Aluno() {
-    return (
-      <div>
-        <h1>Aluno</h1>
-        <p>Este é o componente Aluno</p>
-      </div>
-    );
+import React from "react";
+import {Table} from 'react-bootstrap';
+
+class Aluno extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            alunos: []
+        };
+    }
+
+    render() {
+        return (
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Opcoes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Luiz</td>
+                        <td>email@email.com</td>
+                        <td>Atualizar | Excluir</td>
+                    </tr>
+                </tbody>
+            </Table>
+        );
   }
-  
-  export default Aluno;
+}
+
+export default Aluno;
